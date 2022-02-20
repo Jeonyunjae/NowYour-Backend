8f15c1e4508710bcafdb9e69f9f5402180e9878d
Jeonyunjae <tir211@daum.net>
2022-02-21 01:11:23 +0900

    초기

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [], //->url 가져오고 함수 실행
+  providers: [],
 })
 export class AppModule {}
