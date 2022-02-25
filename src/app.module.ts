33cabe01d29c1026764c28e90958fde293fbcf36
Jeonyunjae <tir211@daum.net>
2022-02-25 13:05:51 +0900

    apollo server init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,20 @@ import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
 import { LocationModule } from './location/location.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MoviesModule,LocationModule],
-  controllers: [AppController],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    UsersModule
+  ],
+  controllers: [],
   providers: [],
 })
-
 export class AppModule {}
